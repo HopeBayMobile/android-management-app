7c5e1d11751c26e5957453e573a9bb4920bf3861
Aaron <aaron.chu@happygorgi.com>
2016-07-04 11:15:57 +0800

    Fix image display issue

--- app/src/main/java/com/hopebaytech/hcfsmgmt/utils/TODO.java
+++
@@ -1,9 +0,0 @@
-package com.hopebaytech.hcfsmgmt.utils;
-
-public class TODO {
-    /**
-     * Next release version 1.0.0036
-     * 1. Implement switch google account feature
-     * 2.
-     */
-}
