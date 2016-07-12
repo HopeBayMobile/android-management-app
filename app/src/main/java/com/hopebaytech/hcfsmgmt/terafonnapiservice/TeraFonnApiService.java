baadd4db2b6dfe8deb04c9fadd16f1995617bf34
Vince.Chiang <vince.chiang@hopebaytech.com>
2016-07-12 16:18:54 +0800

    get mgmt server token

--- app/src/main/java/com/hopebaytech/hcfsmgmt/terafonnapiservice/TeraFonnApiService.java
+++ app/src/main/java/com/hopebaytech/hcfsmgmt/terafonnapiservice/TeraFonnApiService.java
@@ -63,7 +63,7 @@ public class TeraFonnApiService extends Service {
                                 String imei = HCFSMgmtUtils.getDeviceImei(TeraFonnApiService.this);
                                 try {
                                     mGetJWTandIMEIListener.onDataGet(imei, jwt);
-                                } catch (RemoteException e) {
+                                } catch (Exception e) {
                                     e.printStackTrace();
                                 }
                             }
@@ -72,7 +72,7 @@ public class TeraFonnApiService extends Service {
                             public void onFetchFailed() {
                                 try {
                                     mGetJWTandIMEIListener.onDataGet("", "");
-                                } catch (RemoteException e) {
+                                } catch (Exception e) {
                                     e.printStackTrace();
                                 }
                             }
