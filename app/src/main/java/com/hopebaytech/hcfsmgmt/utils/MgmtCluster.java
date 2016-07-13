7fb48e7d381ec07f036eb8e15614b9643464343c
Aaron <aaron.chu@happygorgi.com>
2016-07-13 12:00:24 +0800

    Rename getMgmtServerToken() to getJwtToken(), move IFetchJwtTokenListener into MgmtCluster.java

--- app/src/main/java/com/hopebaytech/hcfsmgmt/utils/MgmtCluster.java
+++ app/src/main/java/com/hopebaytech/hcfsmgmt/utils/MgmtCluster.java
@@ -618,6 +618,22 @@ public class MgmtCluster {
 
     }
 
+    /**
+     * Listener for fetching available JWT token from MGMT server
+     *
+     * @author Aaron
+     *         Created by Aaron on 2016/7/11.
+     */
+    public interface FetchJwtTokenListener {
+
+        /** Callback function when fetch successful */
+        void onFetchSuccessful(String jwtToken);
+
+        /** Callback function when fetch failed */
+        void onFetchFailed();
+
+    }
+
     public interface RegisterListener {
 
         void onRegisterSuccessful(RegisterResultInfo registerResultInfo);
@@ -740,7 +756,9 @@ public class MgmtCluster {
                 @Override
                 public void run() {
                     Handler uiHandler = new Handler(Looper.getMainLooper());
-                    final RegisterResultInfo registerResultInfo = register(authParam, jwtToken);
+//                    final AuthResultInfo authResultInfo = MgmtCluster.auth(authParam);
+//                    if (authResultInfo.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    final RegisterResultInfo registerResultInfo = MgmtCluster.register(authParam, jwtToken);
                     Logs.d(CLASSNAME, "register", "authResultInfo=" + registerResultInfo);
                     if (registerResultInfo.getResponseCode() == HttpsURLConnection.HTTP_OK) {
                         uiHandler.post(new Runnable() {
@@ -855,9 +873,6 @@ public class MgmtCluster {
         return isVerified;
     }
 
-    /**
-     * Get an available JWT token from MGMT server
-     */
     public static void getJwtToken(final Context context, final FetchJwtTokenListener listener) {
         new Thread(new Runnable() {
             @Override
