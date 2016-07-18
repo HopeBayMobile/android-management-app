a1b784d9cd3d0c6e93b2d5f73cbfebd3132e61e9
Aaron <aaron.chu@happygorgi.com>
2016-07-18 16:37:27 +0800

    Integrate with polling service

--- app/src/main/java/com/hopebaytech/hcfsmgmt/service/MgmtPollingService.java
+++ app/src/main/java/com/hopebaytech/hcfsmgmt/service/MgmtPollingService.java
@@ -121,7 +121,6 @@ public class MgmtPollingService extends Service {
                                     }
                                 });
                                 proxy.get();
-
                             }
 
                             @Override
