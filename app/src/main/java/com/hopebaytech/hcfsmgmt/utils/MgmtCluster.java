27d198de80b361fc0a01e1b16935d9236b5de7ad
Aaron <aaron.chu@happygorgi.com>
2016-07-18 12:17:37 +0800

    Add unit test for transfer device

--- app/src/main/java/com/hopebaytech/hcfsmgmt/utils/MgmtCluster.java
+++ app/src/main/java/com/hopebaytech/hcfsmgmt/utils/MgmtCluster.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
+import android.support.annotation.Nullable;
 import android.util.Log;
 
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
@@ -623,19 +624,6 @@ public class MgmtCluster {
 
     }
 
-    /**
-     * Listener for fetching available JWT token from MGMT server
-     */
-    public interface FetchJwtTokenListener {
-
-        /** Callback function when fetch successful */
-        void onFetchSuccessful(String jwtToken);
-
-        /** Callback function when fetch failed */
-        void onFetchFailed();
-
-    }
-
     public interface RegisterListener {
 
         void onRegisterSuccessful(RegisterResultInfo registerResultInfo);
@@ -758,9 +746,7 @@ public class MgmtCluster {
                 @Override
                 public void run() {
                     Handler uiHandler = new Handler(Looper.getMainLooper());
-//                    final AuthResultInfo authResultInfo = MgmtCluster.auth(authParam);
-//                    if (authResultInfo.getResponseCode() == HttpURLConnection.HTTP_OK) {
-                    final RegisterResultInfo registerResultInfo = MgmtCluster.register(authParam, jwtToken);
+                    final RegisterResultInfo registerResultInfo = register(authParam, jwtToken);
                     Logs.d(CLASSNAME, "register", "authResultInfo=" + registerResultInfo);
                     if (registerResultInfo.getResponseCode() == HttpsURLConnection.HTTP_OK) {
                         uiHandler.post(new Runnable() {
