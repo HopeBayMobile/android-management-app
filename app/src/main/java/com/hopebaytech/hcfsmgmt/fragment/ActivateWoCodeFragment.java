31c734cdc4eb7cc53c71cb4fc4cf730aecc6b62f
Aaron <aaron.chu@happygorgi.com>
2016-07-18 17:28:54 +0800

    Automatically convert activation code to uppercase

--- app/src/main/java/com/hopebaytech/hcfsmgmt/fragment/ActivateWoCodeFragment.java
+++ app/src/main/java/com/hopebaytech/hcfsmgmt/fragment/ActivateWoCodeFragment.java
@@ -24,6 +24,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
+import android.text.InputFilter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
