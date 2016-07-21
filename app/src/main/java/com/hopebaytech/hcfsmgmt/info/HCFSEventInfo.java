3db17fdd9ebac99c008163b186323eda17fde90d
Vince.Chiang <vince.chiang@hopebaytech.com>
2016-07-21 11:19:23 +0800

    add tera api server

---
+++ app/src/main/java/com/hopebaytech/hcfsmgmt/info/HCFSEventInfo.java
@@ -0,0 +1,10 @@
+package com.hopebaytech.hcfsmgmt.info;
+
+/**
+ * @author Vince
+ *         Created by Vince on 2016/7/19.
+ */
+
+public class HCFSEventInfo {
+    public static final int TEST = 0;
+}
