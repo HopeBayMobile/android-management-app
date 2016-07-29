6d28ed5b0b93d6b8225888d7c9f0c4dceb49802f
Aaron <aaron.chu@happygorgi.com>
2016-07-29 10:03:10 +0800

    Refresh token only when network is connected

--- app/src/main/java/com/hopebaytech/hcfsmgmt/info/HCFSEventInfo.java
+++ app/src/main/java/com/hopebaytech/hcfsmgmt/info/HCFSEventInfo.java
@@ -8,6 +8,8 @@ public class HCFSEventInfo {
 
     public static final int TEST = 0;
     public static final int TOKEN_EXPIRED = 1;
+
+    // TODO The following event_id is not defined
     public static final int UPLOAD_COMPLETED = 2;
     public static final int EXCEED_PIN_MAX = 3;
 
