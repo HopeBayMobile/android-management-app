06b5fe03b3e1203996b8d662f7e9a33134acb0af
Aaron <aaron.chu@happygorgi.com>
2016-08-03 10:06:15 +0800

    Remove STATUS_UNAVAILABLE_WAIT_TO_DOWNLOAD and STATUS_UNAVAILABLE_NONE_NETWORK

--- app/src/main/java/com/hopebaytech/hcfsmgmt/info/ItemStatus.java
+++ app/src/main/java/com/hopebaytech/hcfsmgmt/info/ItemStatus.java
@@ -8,7 +8,5 @@ public class ItemStatus {
 
     public static final int STATUS_AVAILABLE = 0;
     public static final int STATUS_UNAVAILABLE = 1;
-    public static final int STATUS_UNAVAILABLE_WAIT_TO_DOWNLOAD = 1;
-    public static final int STATUS_UNAVAILABLE_NONE_NETWORK = 2;
 
 }
