f6c7dd1ab24bbd9656c645d5daeb225a27c9be8a
Aaron <aaron.chu@happygorgi.com>
2016-09-08 11:11:19 +0800

    Change the hcfs event id

--- app/src/main/java/com/hopebaytech/hcfsmgmt/utils/HCFSEvent.java
+++ app/src/main/java/com/hopebaytech/hcfsmgmt/utils/HCFSEvent.java
@@ -9,10 +9,12 @@ public class HCFSEvent {
     public static final int TEST = 0;
     public static final int TOKEN_EXPIRED = 1;
     public static final int UPLOAD_COMPLETED = 2;
-    public static final int EXCEED_PIN_MAX = 3;
-    public static final int SPACE_NOT_ENOUGH = 4;
-    public static final int RESTORE_STAGE_1 = 5;
-    public static final int RESTORE_STAGE_2 = 6;
+    public static final int RESTORE_STAGE_1 = 3;
+    public static final int RESTORE_STAGE_2 = 4;
+
+    // The following event is not implemented.
+    public static final int EXCEED_PIN_MAX = 5;
+    public static final int SPACE_NOT_ENOUGH = 6;
 
     public static class ErrorCode {
 
