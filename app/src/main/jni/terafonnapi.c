aec7429cd4eadcd0771fe57553db8af3801ece1f
kewei <kewei.chen@happygorgi.com>
2017-03-29 10:58:46 +0800

    add sharede lib HCFS_api

--- app/src/main/jni/terafonnapi.c
+++ app/src/main/jni/terafonnapi.c
@@ -35,8 +35,9 @@ extern void HCFS_trigger_unboost(const char **json_res);
 extern void HCFS_clear_booster_package_remaining(const char **json_res, const char *package_name);
 extern void HCFS_mount_smart_cache(const char **json_res);
 extern void HCFS_umount_smart_cache(const char **json_res);
-extern void HCFS_check_minimal_apk(const char **json_res, const char *package_path, int blocking);
-extern void HCFS_create_minimal_apk(const char **json_res, const char *package_path, int blocking);
+//extern void HCFS_check_minimal_apk(const char **json_res, const char *package_path, int blocking);
+//extern void HCFS_create_minimal_apk(const char **json_res, const char *package_path, int blocking);
+extern void HCFS_retry_backend(const char **json_res);
 
 //extern void HCFS_collect_sys_logs(const char **json_res);
 
@@ -392,7 +393,7 @@ JNIEXPORT jstring JNICALL Java_com_hopebaytech_hcfsmgmt_utils_HCFSApiUtils_mount
     return result;
 }
 
-JNIEXPORT jstring JNICALL Java_com_hopebaytech_hcfsmgmt_utils_HCFSApiUtils_createMinimalApk(
+/*JNIEXPORT jstring JNICALL Java_com_hopebaytech_hcfsmgmt_utils_HCFSApiUtils_createMinimalApk(
  		JNIEnv *jEnv, jobject jObject, jstring jPkgPath, jint jBlocking) {
  	const char *json_res;
     const char *pkg_path = (*jEnv)->GetStringUTFChars(jEnv, jPkgPath, 0);
@@ -414,7 +415,7 @@ JNIEXPORT jstring JNICALL Java_com_hopebaytech_hcfsmgmt_utils_HCFSApiUtils_check
     free((char *)json_res);
     free((char *)pkg_path);
     return result;
-}
+}*/
 
 JNIEXPORT jstring JNICALL Java_com_hopebaytech_hcfsmgmt_utils_HCFSApiUtils_retryConn(
 		JNIEnv *jEnv, jobject jObject) {
